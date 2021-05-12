fd3a66803bf2eb8198c3bea88f4f186d2535a23c
takahiroyoshikura <junchron@icloud.com>
2021-05-12 14:49:31 +0900

    task1

--- W08/task1_main.js
+++ W08/task1_main.js
@@ -6,7 +6,7 @@ d3.csv("https://takahiroyoshikura.github.io/InfoVis2021_2/W08/task1_data.csv")
 			parent: '#drawing_region',
 			width: 256,
 			height: 128,
-			margin: {top:10, right:10, bottom:20. left:60},
+			margin: {top:10, right:10, bottom:20, left:60}
 		};
 
 		const bar_chart = new BarChart( config, data );
