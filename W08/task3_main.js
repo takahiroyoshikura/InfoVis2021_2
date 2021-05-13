38a62ed5f01cad88759d778ab79b457ba999715e
takahiroyoshikura <junchron@icloud.com>
2021-05-13 19:49:29 +0900

    task3

--- W08/task3_main.js
+++ W08/task3_main.js
@@ -1,6 +1,6 @@
 d3.csv("https://takahiroyoshikura.github.io/InfoVis2021_2/W08/task1_data.csv")
 	.then( data => {
-		data.forEach( d => { d.label = d.label; d.value = +d.value; });
+		data.forEach( d => { d.label = d.label; d.value = +d.value; d.color = d.color;});
 
 		var config = {
 			parent: '#drawing_region',
